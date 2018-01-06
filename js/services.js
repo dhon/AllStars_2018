2f6a689b7b59b3b1f03ad03324e2de2156507daf
dhon <morgangrimes14@gmail.com>
2018-01-05 18:53:23 -0600

    game2

--- js/services.js
+++ js/services.js
@@ -10,7 +10,7 @@ angular.module('app.services', [])
     }
 
     function getJSON(){
-        for(var i = 0; i < 1; i++){
+        for(var i = 0; i < 2; i++){
             var ourRequest = new XMLHttpRequest();
             var num = (i+1);
             var URL = 'data/' + num + '.json'
