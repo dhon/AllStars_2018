88e44b45ad99ca5a00a840da665ccda4d19bfe85
dhon <morgangrimes14@gmail.com>
2018-02-08 01:57:41 -0600

    Add week 6

--- js/services.js
+++ js/services.js
@@ -13,7 +13,7 @@ angular.module('app.services', [])
     };
 
     function getJSON(){
-        for(var i = 0; i < 10; i++){
+        for(var i = 0; i < 12; i++){
             var ourRequest = new XMLHttpRequest();
             var num = (i+1);
             var URL = 'data/' + num + '.json'
