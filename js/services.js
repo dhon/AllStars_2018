607e37947b0faf007fc15b7dbeaabb24957ec6d7
dhon <morgangrimes14@gmail.com>
2018-06-13 20:13:54 -0500

    Add games 41/42

--- js/services.js
+++ js/services.js
@@ -13,7 +13,7 @@ angular.module('app.services', [])
     };
 
     function getJSON(){
-        for(var i = 0; i < 40; i++){
+        for(var i = 0; i < 42; i++){
             var ourRequest = new XMLHttpRequest();
             var num = (i+1);
             var URL = 'data/' + num + '.json'
